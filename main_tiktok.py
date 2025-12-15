da60131ce4aec6e93c8d075ab25cb4d1a51bdc03
lamphat344961-alt <lamphat.344961@gmail.com>
2025-12-15 15:54:35 +0700

    khởi tạo driver

---
+++ main_tiktok.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import pandas as pd
+import random
+import re
+import os
+
+# --- 1. CẤU HÌNH --- ( Tinh chỉnh theo cá nhân)
+MY_PROFILE_PATH = r"c:\Users\Admin\AppData\Roaming\Mozilla\Firefox\Profiles\3k9cekk1.default-release" 
+GECKO_PATH = r"C:\Users\Admin\Desktop\TANPHAT\Manguonmotrongkhoahocjdulieu\DOAN_MNM\tiktok\geckodriver.exe"
+FIREFOX_BINARY_PATH = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+#---------------------------
+
+
+# --- 3. KHỞI TẠO DRIVER ---
+ser = Service(GECKO_PATH)
+options = webdriver.firefox.options.Options()
+options.binary_location = FIREFOX_BINARY_PATH
+options.add_argument("-profile")
+options.add_argument(MY_PROFILE_PATH)
+options.set_preference("dom.webdriver.enabled", False)
+options.set_preference("useAutomationExtension", False)
+
+driver = None 
+
+print("WARNING: Hãy TẮT HOÀN TOÀN Firefox thật trước khi chạy!")
+print("Đang khởi động Firefox với Profile cá nhân...")
+
+driver = webdriver.Firefox(options=options, service=ser)
+wait = WebDriverWait(driver, 20)
+action = ActionChains(driver)
+
+# Vào trang Ads
+target_url = "https://ads.tiktok.com/creative/forpartners/creator/explore?region=row"
+print(f"Truy cập: {target_url}")
+driver.get(target_url)
+driver.maximize_window()
+time.sleep(5) 
+print("Tiêu đề trang hiện tại:", driver.title)
